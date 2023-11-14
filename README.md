02c142fa3cffec2a18e0cffa84bfa974f1f01972
Yuya Sakamoto <yuya.s713@gmail.com>
2023-11-14 19:00:13 +0900

    maked バリデータ

--- README.md
+++ README.md
@@ -3,12 +3,15 @@ File Manipulator Program
 ## Memo
 1.ファイルの読み書きとシェルの引数入力の取り方を学ぶために'file_manipular'を作成する
 
-2. CLI'main.py [command] [option] [option]'と入力すれば動く
+2.CLI'main.py [command] [option] [option]'と入力すれば動く
 
-## file_manipulator.pyについて
+3.バリデータの入力もしてあります。
+
+4.classの代わりにファイルで分けて管理してあります。
+## file_manipulatorについて
 - 'reverse inputpath outputpath'
 inputpathにあるファイルを受取、outputpathにinputpathの内容を逆にした新しいファイルを作成します。
-- 'copy inputpath outputpath' 
+- 'copy inputpath outputpath'
 inputpathにあるファイルのコピーを作成し、outputpathとして保存します
 - 'duplicate-contents inputpath n'
 inputpathにあるファイルの内容を読み込み、その内容を複製し、複製された内容をinputpathにn回複製します。
--- main/action.py
+++ main/action.py
@@ -1,62 +1,94 @@
+'''
+'reverse inputpath outputpath'
+'copy inputpath outputpath'
+'duplicate-contents inputpath n'
+'replace-string inputpath needle newstring'
+arr[0]: main.py
+arr[1]: command
+arr[2:]: other
+'''
 def reverse(inputPath, outputPath):
-    print('inputPath :', inputPath)
     print('outputPath :', outputPath)
-    content = ""
-    with open(inputPath, "r") as f:
-        lines = f.read()
-        content += lines[::-1]
-        print(content)
+    try:
+        content = ""
+        with open(inputPath, "r") as f:
+            lines = f.read()
+            content += lines[::-1]
+            print(content)
 
-    with open(outputPath, "w") as f:
-        f.write(content)
+        with open(outputPath, "w") as f:
+            f.write(content)
+    except Exception as e:
+        print(e)
+        print(type(e))
 
 def copy(inputPath, outputPath):
-    print('inputPath :', inputPath)
     print('outputPath :', outputPath)
-    content = ""
-    with open(inputPath, "r") as f:
-        content += f.read()
-        print(content)
+    try:
+        content = ""
+        with open(inputPath, "r") as f:
+            content += f.read()
+            print(content)
 
-    with open(outputPath, "w") as f:
-        f.write(content)
+        with open(outputPath, "w") as f:
+            f.write(content)
+    except Exception as e:
+        print(e)
+        print(type(e))
 
 def duplicate_contents(inputPath, num):
-    content = ""
-    with open(inputPath, "r") as f:
-        s += f.read()
-        for _ in range(int(num)):
-            content += s + '\n'
-        print(content[:-1])
+    print("num :", num)
+    try:
+        num = int(num)
+        content = ""
+        with open(inputPath, "r") as f:
+            s = f.read()
+            for _ in range(int(num)):
+                content += s + '\n'
+            print(content[:-1])
 
-    with open(inputPath, "w") as f:
-        f.write(content[:-1])
+        with open(inputPath, "w") as f:
+            f.write(content[:-1])
+    except Exception as e:
+        print(e)
+        print(type(e))
 
 def replace_string(inputPath, needle, newString):
-    content = ""
-    with open(inputPath, "r") as f:
-        content += f.read()
-        print(content)
-        content = content.replace(needle, newString)
-        print(content)
+    print("needle :", needle)
+    print("new string:", newString)
+    try:
+        content = ""
+        with open(inputPath, "r") as f:
+            content += f.read()
+            print(content)
+            content = content.replace(needle, newString)
+            print(content)
 
-    with open(inputPath, "w") as f:
-        f.write(content)
+        with open(inputPath, "w") as f:
+            f.write(content)
+    except Exception as e:
+        print(e)
+        print(type(e))
 
 def actSome(arr):
     print(arr)
-    print("command :", arr[1])
-    if arr[1] in ('reverse', '-rev'):
-        print('reverse on')
-        reverse(arr[2],arr[3])
-    elif arr[1] in ('copy', '-c'):
-        print('copy on')
-        copy(arr[2],arr[3])
-    elif arr[1] in ('duplicate', '-d'):
-        print('duplicate-contents on')
-        duplicate_contents(arr[2],arr[3])
-    elif arr[1] in ('replace', '-rep'):
-        print('replace-string on')
-        replace_string(arr[2], arr[3], arr[4])
-    else:
-        print('not found')
+    print("command :", arr[0])
+    print('inputPath :', arr[1])
+    try:
+        if arr[0] in ('reverse', '-rev'):
+            print('reverse on')
+            reverse(arr[1],arr[2])
+        elif arr[0] in ('copy', '-c'):
+            print('copy on')
+            copy(arr[1],arr[2])
+        elif arr[0] in ('duplicate', '-d'):
+            print('duplicate-contents on')
+            duplicate_contents(arr[1],arr[2])
+        elif arr[0] in ('replace', '-rep'):
+            print('replace-string on')
+            replace_string(arr[1], arr[2], arr[3])
+        else:
+            print('not found')
+    except Exception as e:
+        print(e)
+        print(type(e))
